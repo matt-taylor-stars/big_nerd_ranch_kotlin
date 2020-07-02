327327212518ee850966767f3434d9c2dc318103
matt <maffyoo@gmail.com>
2020-07-02 20:36:08 +0100

    Saved additional work on exercises

--- Sandbox/src/TypeIntro.kt
+++ Sandbox/src/TypeIntro.kt
@@ -2,7 +2,12 @@ const val MAX_EXPERIENCE: Int = 5000
 
 fun main(args: Array<String>) {
     val playerName = "Estragon"
-    var experiencePoints: Int = 5
+    var experiencePoints = 5
+    var hasSteed = false
+    var pubName: String = "Unicorn's Horn"
+    var publicanOnDuty: String = ""
+    var gold: Int = 0
+    val menu=
     experiencePoints += 5
     println(experiencePoints)
     println(playerName)
